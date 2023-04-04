5674fb4197c1d77e1efe3498966fff4a538ac9d6
leonkk9320 <1060094441@qq.com>
2023-04-04 00:10:30 -0700

    add chat functionality

--- Application.py
+++ Application.py
@@ -1,6 +1,10 @@
 import joblib
 import streamlit as st
 
+import openai
+from streamlit_chat import message
+import os
+openai.api_key = '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'
 
 class Application:
     def __init__(self):
@@ -16,6 +20,42 @@ class Application:
                 st.write("Prediction: ", prediction)
             except Exception as e:
                 st.write("Invalid feature array. Need 34 comma separated values.")
+
+        #storing the chat
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = []
+        if 'past' not in st.session_state:
+            st.session_state['past'] = []
+        user_input=st.text_input("You:",key='input')
+        if user_input:
+            completion=openai.Completion.create(
+                engine='text-davinci-003',
+                prompt=user_input,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.6,
+            )
+            output=completion.choices[0].text
+            #store the output
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+        if st.session_state['generated']:
+            for i in range(len(st.session_state['generated'])-1, -1, -1):
+                message(st.session_state["generated"][i], key=str(i))
+                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+    # def generate_response(prompt):
+    #     completion=openai.Completion.create(
+    #         engine='text-davinci-003',
+    #         prompt=prompt,
+    #         max_tokens=1024,
+    #         n=1,
+    #         stop=None,
+    #         temperature=0.6,
+    #     )
+    #     message=completion.choices[0].text
+    #     return message
 
 
 if __name__ == "__main__":
